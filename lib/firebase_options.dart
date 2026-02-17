4992b30ea2fca1937cfc29638afbac865b21736f
dhairyajangir <thinkelite@hotmail.com>
2026-02-17 23:28:16 +0530

    deleting the file

--- lib/firebase_options.dart
+++
@@ -1,8 +0,0 @@
-// This file is a placeholder. Run `flutterfire configure` to generate
-// platform-specific Firebase options and replace this file.
-import 'package:firebase_core/firebase_core.dart';
-
-class DefaultFirebaseOptions {
-  static FirebaseOptions get currentPlatform => throw UnsupportedError(
-      'DefaultFirebaseOptions not configured. Run `flutterfire configure` to generate this file.');
-}
