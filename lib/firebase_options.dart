765470fca3f1a856cb43afd145334821be281fce
dhairyajangir <thinkelite@hotmail.com>
2026-02-22 18:44:26 +0530

    feat: update FirebaseOptions to use environment variables for configuration

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,5 +1,13 @@
 // File generated based on Firebase project: sakhi-dc3e3
 // ignore_for_file: lines_longer_than_80_chars, avoid_classes_with_only_static_members
+//
+// Pass credentials at build time with --dart-define or --dart-define-from-file:
+//   flutter run \
+//     --dart-define=FIREBASE_API_KEY=AIzaSy... \
+//     --dart-define=FIREBASE_APP_ID=1:... \
+//     --dart-define=FIREBASE_MESSAGING_SENDER_ID=... \
+//     --dart-define=FIREBASE_PROJECT_ID=... \
+//     --dart-define=FIREBASE_STORAGE_BUCKET=...
 
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
@@ -29,10 +37,10 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDQ5E-P4eCkC2YRR7W1M4bMRYcLuecVMgE',
-    appId: '1:809644915762:android:0faafd6ea6fba055c2902b',
-    messagingSenderId: '809644915762',
-    projectId: 'sakhi-dc3e3',
-    storageBucket: 'sakhi-dc3e3.firebasestorage.app',
+    apiKey: String.fromEnvironment('FIREBASE_API_KEY'),
+    appId: String.fromEnvironment('FIREBASE_APP_ID'),
+    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID'),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
   );
 }
